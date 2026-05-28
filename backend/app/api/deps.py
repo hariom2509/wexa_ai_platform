166bc351ba573891633c38cf1cbd3502bf607c24
Hari Om <om6893162@gmail.com>
2026-05-29 00:14:14 +0530

    fix(backend): fix func import for api key auth

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -3,6 +3,7 @@ from fastapi.security import OAuth2PasswordBearer, APIKeyHeader
 from jose import jwt, JWTError
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
+from sqlalchemy import func
 
 from app.core.config import settings
 from app.core.database import get_db
